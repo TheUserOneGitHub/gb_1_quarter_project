b09ac3bb1538b9803985b39e8a35ca7ec47809bc
TheUserOne <theuserone@yandex.ru>
2023-03-24 19:37:58 +0500

    program v0.1

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int CheckN(int n)
+{
+    while (n == 0 || n < 0)
+    {
+        Console.WriteLine("You have entered 0 or less, there is nothing to be checked");
+        Console.WriteLine("Enter how many elemets you need to check (input an integer)");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+    return n;
+}
+
+void PrintArray(string[] array)
+{
+    int i = 0;
+    int n = array.Length;
+    while (i < n)
+    {
+        Console.Write($"{array[i]}");
+        i++;
+        if (i < n)
+            Console.Write($", ");
+    }
+    Console.WriteLine($"");
+}
+
+
+
+
+
+Console.WriteLine("Enter how many elemets you need to check (input an integer)");
+int n = Convert.ToInt32(Console.ReadLine());
+int k = n;
+n = CheckN(n);
+string[] array = new string[n];
+string[] arrayTemp = new string[k];
+
+Console.WriteLine("Type a 'word' (word, number, symbol, etc) you want to check and press enter one by one");
+int i = 0;
+while (i < n)
+{
+    array[i] = Console.ReadLine();
+    i++;
+}
+
+Console.Write($"You have entered: ");
+PrintArray(array);
+
+i = 0;
+k = 0;
+n = array.Length;
+while (i < n)
+{
+    if (array[i].Length <= 3)
+    {
+        arrayTemp[k] = array[i];
+        k++;
+    }
+    i++;
+}
+
+Console.WriteLine($"arrayTemp.Length = {arrayTemp.Length}");
+
+string[] arrayFinal = new string[k];
+i = 0;
+while (i < k)
+{
+    arrayFinal[i] = arrayTemp[i];
+    i++;
+}
+
+if (k == 0)
+    Console.WriteLine("there is nothing less or equal to 3 symbols");
+
+Console.Write($"Total words with 3 symbols or less is {k}: ");
+PrintArray(arrayFinal);
